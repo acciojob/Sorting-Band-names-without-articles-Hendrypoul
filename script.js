5cf0a746a615f3158281acf08d8479afc35b54ce
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-03-22 00:19:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').trim();
+}
 
+const sortedBans = bands.sort(function (a, b){
+ if(a > b) {
+    return 1;
+ }  else {
+    return -1;
+ }
+})
+
+console.log (sortedBands)
+
+const sortedBands = bands.sort((a,b) => strip(a) > strip(b) ? 1 : -1);
+
+document.querySelector("#bands").innerHTML = 
+  sortedBands
+  .map(band => `<li>${band}</li>`)
+  .join('') 
